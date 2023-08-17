9901cff96bb2518d4d8786f1be04ffd96a1321a6
Rashmi-Bishwokarma <rashmibishwokarma15@gmail.com>
2023-08-17 17:09:25 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:the_flutter_demo/views/pages/home_screen.dart';
+
+void main() {
+  runApp(   GetMaterialApp(
+    home:HomeScreen()
+  ));
+}
